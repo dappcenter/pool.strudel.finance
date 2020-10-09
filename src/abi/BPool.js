94f6041a9a9bdd86fe154b53859c67b8c661dca0
Keno Budde <Clearwood@users.noreply.github.com>
2020-10-09 15:24:09 +0200

    fixed problems

--- src/abi/BPool.js
+++ src/abi/BPool.js
@@ -1,21 +1,23 @@
-const BPool = [{
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "token",
-          "type": "address"
-        }
-      ],
-      "name": "getDenormalizedWeight",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }]
+const BPool = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'token',
+        type: 'address',
+      },
+    ],
+    name: 'getDenormalizedWeight',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
 
 export default BPool;
--- src/setup/tokens.js
+++ src/setup/tokens.js
@@ -4,8 +4,8 @@ import BigNumber from 'bignumber.js';
 import { eth } from '@pie-dao/eth';
 import { ethers } from 'ethers';
 import { mint } from '@pie-dao/mint';
-import { ReservePoolController } from '../abi/ReservePoolController';
-import { BPool } from '../abi/BPool';
+import ReservePoolController from '../abi/ReservePoolController';
+import BPool from '../abi/BPool';
 
 export const controllerAddress = '0xd439932fbc03c0f646324ef09a4a0c28c0d4008b'.toLowerCase();
 export const WETHAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase();
@@ -42,25 +42,21 @@ const buildTokens = (mappedAmounts) => ({
 
 
 const updateTokens = async ({ database }) => {
+  const { signer } = eth;
+  console.log(database);
   try {
-    const [WETHBalance, VBTCBalance] = await Promise.all([
-      database.balance({ address: poolAddress, token: WETHAddress }),
-      database.balance({ address: poolAddress, token: VBTCAddress }),
-    ]);
     const poolContract = new ethers.Contract(
       poolAddress,
       BPool,
-      signer
+      signer,
     );
 
-    const denormWeight = await poolContract.methods
-          .getDenormalizedWeight(vBtcContract.options.address)
-          .call()
-    const weightVBTC  =  new BigNumber(denormWeight)
+    const denormWeight = await poolContract.methods.getDenormalizedWeight(VBTCAddress).call();
+    const weightVBTC = new BigNumber(denormWeight);
 
-    const wethWeight = new BigNumber(5).times(new BigNumber(10e17))
+    const wethWeight = new BigNumber(5).times(new BigNumber(10e17));
 
-    const totalWeight = weightVBTC.plus(wethWeight)
+    const totalWeight = weightVBTC.plus(wethWeight);
 
     const WETHPercentage = wethWeight.dividedBy(totalWeight);
     const VBTCPercentage = weightVBTC.dividedBy(totalWeight);
@@ -86,15 +82,17 @@ const updateTokens = async ({ database }) => {
 };
 
 export const initialize = async ({ database }) => {
-  const { approve, notify, signer, transactionOverrides } = eth;
+  const {
+    approve, notify, signer, transactionOverrides,
+  } = eth;
 
   // Load up pools target weights
   const controllerContract = new ethers.Contract(
     controllerAddress,
     ReservePoolController,
-    signer
+    signer,
   );
-  //const poolAmount = ethers.BigNumber.from('1000000000000000000');
+  // const poolAmount = ethers.BigNumber.from('1000000000000000000');
 
   const [WETHBalance, VBTCBalance] = await Promise.all([
     database.balance({ address: poolAddress, token: WETHAddress }),
@@ -107,7 +105,7 @@ export const initialize = async ({ database }) => {
       '0xe1406825186d63980fd6e2ec61888f7b91c4bae4',
     ],
     [WETHBalance, VBTCBalance],
-  ]; //await controllerContract.calcTokensForAmount(poolAmount);
+  ]; // await controllerContract.calcTokensForAmount(poolAmount);
 
   console.log('poolAmounts:', poolAmounts[1][0].toString());
   const mappedAmounts = {};
@@ -128,7 +126,7 @@ export const initialize = async ({ database }) => {
     await approve({ spender: controllerAddress, token: VBTCAddress });
 
     notify(
-      await controllerContract.joinPool(joinAmount, [MAX, MAX], overrides)
+      await controllerContract.joinPool(joinAmount, [MAX, MAX], overrides),
     );
   };
 
